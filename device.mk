46c463c30cfba20602006d8c78222c840017256d
Angel Castillo <acastillorobles77@gmail.com>
2023-04-07 13:27:44 -0600

    vayu: Build GameSpace

--- device.mk
+++ device.mk
@@ -139,6 +139,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# GameSpace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # Dex
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
